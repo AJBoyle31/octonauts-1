276bdf3ad0f09ff2fb3e7353721154e0c60a70d8
Andrew <ajboyle31@gmail.com>
2020-12-13 15:36:05 -0500

    Add previous level, level 6 wip

--- Levels/NextLevel.gd
+++ Levels/NextLevel.gd
@@ -3,11 +3,11 @@ extends Area2D
 
 
 export var level_number: int
-
+export var previous_level: bool = false
 
 func _on_NextLevel_body_entered(body):
 	var fish_left = get_tree().get_nodes_in_group("Fish").size()
-	if fish_left == 0:
+	if previous_level || fish_left == 0:
 		get_tree().change_scene("res://Levels/Level" + str(level_number) + ".tscn")
 	
 
